a07e1e987838f57e60f841da5003fc47b1253d07
Rahul Pandey <pandeyrahul813684@gmail.com>
2024-05-04 20:54:32 +0530

    first commit

---
+++ frontend/src/utils/functions.js
@@ -0,0 +1,48 @@
+// function to format date present in github api response to 'Month Day, Year' format (e.g. 'January 1st, 2022') and return it as a string .
+export function formatMemberSince(inputDateString) {
+	const options = { month: "short", day: "2-digit", year: "numeric" };
+	const formattedDate = new Date(inputDateString).toLocaleDateString("en-US", options);
+	return formattedDate;
+}
+
+export function formatDate(inputDateString) {
+	const months = [
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December",
+	];
+
+	const date = new Date(inputDateString);
+	const monthName = months[date.getMonth()];
+	const day = date.getDate();
+	const year = date.getFullYear();
+
+	// Function to add ordinal suffix to day
+	function getOrdinalSuffix(day) {
+		if (day >= 11 && day <= 13) {
+			return day + "th";
+		}
+		switch (day % 10) {
+			case 1:
+				return day + "st";
+			case 2:
+				return day + "nd";
+			case 3:
+				return day + "rd";
+			default:
+				return day + "th";
+		}
+	}
+
+	const formattedDate = `${monthName} ${getOrdinalSuffix(day)}, ${year}`;
+	return formattedDate;
+}
